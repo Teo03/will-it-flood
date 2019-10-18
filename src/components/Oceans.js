954153606149008511107e0862dd6b3c84d65580
Teo <teodorbogoeski@gmail.com>
2019-10-18 17:16:43 +0200

    added routing

---
+++ src/components/Oceans.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Oceans() {
+  return (
+    <div className="Oceans">
+        <p>Oceans</p>
+    </div>
+  );
+}
+
+export default Oceans;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Search() {
+  return (
+    <div className="Search">
+        <p>search</p>
+    </div>
+  );
+}
+
+export default Search;
