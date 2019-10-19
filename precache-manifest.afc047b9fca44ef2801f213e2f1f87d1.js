50e67f6e4404fa23eaf57fd7514021ec725f0a3b
Teo <teodorbogoeski@gmail.com>
2019-10-19 18:10:50 +0200

    Updates

---
+++ precache-manifest.afc047b9fca44ef2801f213e2f1f87d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a5342c96639ba3d007fdd0c7dfda45",
+    "url": "/will-it-flood/index.html"
+  },
+  {
+    "revision": "747fc8594fd103a59f04",
+    "url": "/will-it-flood/static/css/2.dba924d2.chunk.css"
+  },
+  {
+    "revision": "e74f7fbd223deb46867a",
+    "url": "/will-it-flood/static/css/main.294a9136.chunk.css"
+  },
+  {
+    "revision": "747fc8594fd103a59f04",
+    "url": "/will-it-flood/static/js/2.567b4298.chunk.js"
+  },
+  {
+    "revision": "e74f7fbd223deb46867a",
+    "url": "/will-it-flood/static/js/main.e025776c.chunk.js"
+  },
+  {
+    "revision": "b838f7676d283fa00ab4",
+    "url": "/will-it-flood/static/js/runtime-main.144dced0.js"
+  },
+  {
+    "revision": "a7910652e23ba235a8752d0ae384dea6",
+    "url": "/will-it-flood/static/media/cfc.a7910652.png"
+  },
+  {
+    "revision": "eb23556d6d0fa1c3b8c8a85cc48b2750",
+    "url": "/will-it-flood/static/media/ch4.eb23556d.png"
+  },
+  {
+    "revision": "ae4c8813a2053a4d10125e8372870113",
+    "url": "/will-it-flood/static/media/co2.ae4c8813.png"
+  },
+  {
+    "revision": "9137fa03e40607c84947c7236fbdd420",
+    "url": "/will-it-flood/static/media/h2o.9137fa03.png"
+  },
+  {
+    "revision": "7f9904e21629e9ea357269824195e458",
+    "url": "/will-it-flood/static/media/logo.7f9904e2.png"
+  },
+  {
+    "revision": "d7900490d1f6e913b2bc2972f0542f01",
+    "url": "/will-it-flood/static/media/n2o.d7900490.png"
+  }
+]);
